37d27bbd4caf5301dae2c76bfd11ec8eddcc3749
Song Luo <luos@rose-hulman.edu>
2021-04-27 23:52:40 -0500

    initial commit

---
+++ src/Collector.py
@@ -0,0 +1,17 @@
+import torch
+import numpy as np
+import ray
+
+class Collector:
+    def __init__(self, args=None):
+        self.env = None
+        self.buffer = None
+        self.reward_scale = None
+        self.gamma = None
+
+
+    def explore(self,)-> int:
+
+        return 1
+    
+    
---
+++ src/Critic.py
@@ -0,0 +1,11 @@
+import torch
+import numpy as np
+
+class Critic:
+    def __init__(self, args=None):
+        self.learning_rate = 1e-4 if args is None else args['learning_rate']
+        self.soft_update_tau = 2 ** -8 if args is None else args['soft_update_tau']  # 5e-3 ~= 2 ** -8
+        self.model = None
+        self.optimizer = None
+        self.criterion = None
+        
